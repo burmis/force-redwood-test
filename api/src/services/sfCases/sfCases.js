f6378f047c4927f4c9b3f5c3093234e33e429933
Mark Bennett <mark@burmis.ca>
2022-01-05 18:16:56 +0000

    First pass at retrieving data from Salesforce.

--- api/src/services/sfCases/sfCases.js
+++ api/src/services/sfCases/sfCases.js
@@ -1,3 +1,39 @@
+import { Connection } from 'jsforce'
+
+const ACCESS_TOKEN = `${process.env.SALESFORCE_ACCESS_TOKEN}`
+const INSTANCE_URL = `${process.env.SALESFORCE_INSTANCE_URL}`
+
+/**
+ * Retrieve a valid access token from Salesforce.
+ *
+ * Currently hits the OAuth2 token endpoint using the password grant type.
+ *
+ * @see https://developer.salesforce.com/docs/atlas.en-us.api_rest.meta/api_rest/intro_understanding_grant_types.htm
+ * @see https://help.salesforce.com/s/articleView?id=sf.remoteaccess_oauth_endpoints.htm&type=5
+ * @see https://help.salesforce.com/s/articleView?id=sf.remoteaccess_oauth_username_password_flow.htm&type=5
+ */
+async function getToken() {
+  // TODO: Actually used the client secrets, etc to get a fresh token
+  return ACCESS_TOKEN
+}
+
+async function getConnection() {
+  const conn = new Connection({
+    instanceUrl: INSTANCE_URL,
+    accessToken: await getToken(),
+  })
+  return conn
+}
+
+async function getCases() {
+  const conn = await getConnection()
+  const result = await conn.query(
+    'SELECT Id, AccountId, Account.Name, CaseNumber, Status, Description, Subject FROM Case'
+  )
+  console.log('👀 query result = ', result)
+  return cases
+}
+
 const cases = [
   {
     id: '1',
@@ -11,7 +47,8 @@ const cases = [
 ]
 
 export const sfCases = () => {
-  return cases
+  const conn = getConnection()
+  return getCases(conn)
 }
 
 export const sfCase = ({ id }) => {
