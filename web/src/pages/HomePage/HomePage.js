975553381ce875802e7d6e27c7b9bd11ccd2943f
Mark Bennett <mark@burmis.ca>
2022-01-06 13:43:17 +0000

    Add a form we can use to filter cases.

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,8 +1,12 @@
-import { Link, routes } from '@redwoodjs/router'
+import { Form, Label, Submit, TextField } from '@redwoodjs/forms'
 import { MetaTags } from '@redwoodjs/web'
 import CasesCell from 'src/components/CasesCell'
 
 const HomePage = () => {
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
   return (
     <>
       <MetaTags
@@ -12,7 +16,26 @@ const HomePage = () => {
 
       <h1>Salesforce + Redwood = ❤️</h1>
 
-      <p>Showing cases owned by &quot;sarah@test.com&quot;.</p>
+      <p>
+        Filter the displayed cases by email. In production, this would be tied
+        to a users email or other values set by the team.
+      </p>
+      <Form onSubmit={onSubmit}>
+        <Label
+          name="contactEmail"
+          className="label"
+          errorClassName="label error"
+        >
+          Client Email:
+        </Label>
+        <TextField
+          name="contactEmail"
+          className="input"
+          errorClassName="input error"
+          validation={{ reqired: true }}
+        />
+        <Submit className="button">Filter</Submit>
+      </Form>
       <CasesCell contactEmail="sarah@test.com" />
     </>
   )
