5e855e333c9b69dabdae21ccabee75fe0c9e7687
Mark Bennett <mark@burmis.ca>
2022-01-06 20:45:48 +0000

    Another pass at some style

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -3,6 +3,7 @@ import { MetaTags } from '@redwoodjs/web'
 import CasesCell from 'src/components/CasesCell'
 import { useState } from 'react'
 import MainLayout from 'src/layouts/MainLayout/MainLayout'
+import { MailIcon } from '@heroicons/react/solid'
 
 const HomePage = () => {
   const [contactEmail, setContactEmail] = useState('')
@@ -30,18 +31,27 @@ const HomePage = () => {
         >
           <Label
             name="contactEmail"
-            className="label"
-            errorClassName="label error"
+            className="block text-sm font-medium text-gray-700"
+            errorClassName="block text-sm font-medium text-gray-700 error"
           >
             Client Email:
           </Label>
-          <TextField
-            name="contactEmail"
-            className="input"
-            errorClassName="input error"
-            validation={{ reqired: true }}
-          />
-          <Submit className="button">Filter</Submit>
+
+          <div className="mt-1 relative rounded-md shadow-sm">
+            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+              <MailIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+            </div>
+            <TextField
+              name="contactEmail"
+              className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-10 sm:text-sm border-gray-300 rounded-md"
+              errorClassName="error focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-10 sm:text-sm border-gray-300 rounded-md"
+              validation={{ reqired: true }}
+              placeholder="you@example.com"
+            />
+          </div>
+          <Submit className="mt-2 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            Filter
+          </Submit>
         </Form>
         <CasesCell contactEmail={contactEmail} />
       </MainLayout>
