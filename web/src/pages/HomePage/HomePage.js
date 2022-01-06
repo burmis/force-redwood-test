7d8665b1f1e577ffdb76cba04e74bf0d83681297
Mark Bennett <mark@burmis.ca>
2022-01-06 20:22:44 +0000

    Apply the MainLayout to the HomePage

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -2,6 +2,7 @@ import { Form, Label, Submit, TextField } from '@redwoodjs/forms'
 import { MetaTags } from '@redwoodjs/web'
 import CasesCell from 'src/components/CasesCell'
 import { useState } from 'react'
+import MainLayout from 'src/layouts/MainLayout/MainLayout'
 
 const HomePage = () => {
   const [contactEmail, setContactEmail] = useState('')
@@ -18,29 +19,31 @@ const HomePage = () => {
         description="An example app using Salesforce with Redwood.js"
       />
 
-      <h1>Salesforce + Redwood = ❤️</h1>
+      <MainLayout>
+        <h1>Salesforce + Redwood = ❤️</h1>
 
-      <p>
-        Filter the displayed cases by email. In production, this would be tied
-        to a users email or other values set by the team.
-      </p>
-      <Form onSubmit={onSubmit}>
-        <Label
-          name="contactEmail"
-          className="label"
-          errorClassName="label error"
-        >
-          Client Email:
-        </Label>
-        <TextField
-          name="contactEmail"
-          className="input"
-          errorClassName="input error"
-          validation={{ reqired: true }}
-        />
-        <Submit className="button">Filter</Submit>
-      </Form>
-      <CasesCell contactEmail={contactEmail} />
+        <p>
+          Filter the displayed cases by email. In production, this would be tied
+          to a users email or other values set by the team.
+        </p>
+        <Form onSubmit={onSubmit}>
+          <Label
+            name="contactEmail"
+            className="label"
+            errorClassName="label error"
+          >
+            Client Email:
+          </Label>
+          <TextField
+            name="contactEmail"
+            className="input"
+            errorClassName="input error"
+            validation={{ reqired: true }}
+          />
+          <Submit className="button">Filter</Submit>
+        </Form>
+        <CasesCell contactEmail={contactEmail} />
+      </MainLayout>
     </>
   )
 }
