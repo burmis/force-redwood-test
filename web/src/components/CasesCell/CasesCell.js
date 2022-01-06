7a810925189de20c4c1325038b4bf35bb9ddf9da
Mark Bennett <mark@burmis.ca>
2022-01-06 13:56:24 -0700

    10 add tailwind and pretty (#12)
    * Initialize tailwind
    
    * Add headless UI
    
    * Create a MainLayout
    
    * Apply the MainLayout to the HomePage
    
    * Add Heroicons
    
    * First pass at new layout and styles.
    
    * Break apart elements.
    
    * Another pass at some style
    
    * Ugly formatting

--- web/src/components/CasesCell/CasesCell.js
+++ web/src/components/CasesCell/CasesCell.js
@@ -1,3 +1,5 @@
+import { MailIcon, PhoneIcon } from '@heroicons/react/solid'
+
 export const QUERY = gql`
   query CasesQuery($contactEmail: String) {
     cases: sfCases(contactEmail: $contactEmail) {
@@ -25,10 +27,71 @@ export const Failure = ({ error }) => (
 
 export const Success = ({ cases }) => {
   return (
-    <ul>
+    <ul className="border border-gray-200 rounded-lg px-2 py-2 my-2">
       {cases.map((item) => {
-        return <li key={item.id}>{JSON.stringify(item)}</li>
+        return (
+          <li
+            className="my-4 border border-gray-200 rounded-lg px-2 py-2"
+            key={item.id}
+          >
+            {JSON.stringify(item)}
+            {/* <CaseCard /> */}
+          </li>
+        )
       })}
     </ul>
   )
 }
+
+// const imageUrl =
+//   'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60'
+
+// function CaseCard() {
+//   return (
+//     <li
+//       key={person.email}
+//       className="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200"
+//     >
+//       <div className="w-full flex items-center justify-between p-6 space-x-6">
+//         <div className="flex-1 truncate">
+//           <div className="flex items-center space-x-3">
+//             <h3 className="text-gray-900 text-sm font-medium truncate">
+//               {person.name}
+//             </h3>
+//             <span className="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">
+//               {person.role}
+//             </span>
+//           </div>
+//           <p className="mt-1 text-gray-500 text-sm truncate">{person.title}</p>
+//         </div>
+//         <img
+//           className="w-10 h-10 bg-gray-300 rounded-full flex-shrink-0"
+//           src={person.imageUrl}
+//           alt=""
+//         />
+//       </div>
+//       <div>
+//         <div className="-mt-px flex divide-x divide-gray-200">
+//           <div className="w-0 flex-1 flex">
+//             <a
+//               href={`mailto:${person.email}`}
+//               className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500"
+//             >
+//               <MailIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
+//               <span className="ml-3">Email</span>
+//             </a>
+//           </div>
+//           <div className="-ml-px w-0 flex-1 flex">
+//             <a
+//               href={`tel:${person.telephone}`}
+//               className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500"
+//             >
+//               <PhoneIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
+//               <span className="ml-3">Call</span>
+//             </a>
+//           </div>
+//         </div>
+//       </div>
+//     </li>
+//   )
+// }
--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -2,6 +2,8 @@ import { Form, Label, Submit, TextField } from '@redwoodjs/forms'
 import { MetaTags } from '@redwoodjs/web'
 import CasesCell from 'src/components/CasesCell'
 import { useState } from 'react'
+import MainLayout from 'src/layouts/MainLayout/MainLayout'
+import { MailIcon } from '@heroicons/react/solid'
 
 const HomePage = () => {
   const [contactEmail, setContactEmail] = useState('')
@@ -18,29 +20,41 @@ const HomePage = () => {
         description="An example app using Salesforce with Redwood.js"
       />
 
-      <h1>Salesforce + Redwood = ❤️</h1>
-
-      <p>
-        Filter the displayed cases by email. In production, this would be tied
-        to a users email or other values set by the team.
-      </p>
-      <Form onSubmit={onSubmit}>
-        <Label
-          name="contactEmail"
-          className="label"
-          errorClassName="label error"
+      <MainLayout>
+        <p className="mx-auto py-2">
+          Filter the displayed cases by email. In production, this would be tied
+          to a users email or other values set by the team.
+        </p>
+        <Form
+          className="border border-gray-200 rounded-lg px-2 py-2 my-2"
+          onSubmit={onSubmit}
         >
-          Client Email:
-        </Label>
-        <TextField
-          name="contactEmail"
-          className="input"
-          errorClassName="input error"
-          validation={{ reqired: true }}
-        />
-        <Submit className="button">Filter</Submit>
-      </Form>
-      <CasesCell contactEmail={contactEmail} />
+          <Label
+            name="contactEmail"
+            className="block text-sm font-medium text-gray-700"
+            errorClassName="block text-sm font-medium text-gray-700 error"
+          >
+            Client Email:
+          </Label>
+
+          <div className="mt-1 relative rounded-md shadow-sm">
+            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+              <MailIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+            </div>
+            <TextField
+              name="contactEmail"
+              className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-10 sm:text-sm border-gray-300 rounded-md"
+              errorClassName="error focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-10 sm:text-sm border-gray-300 rounded-md"
+              validation={{ reqired: true }}
+              placeholder="you@example.com"
+            />
+          </div>
+          <Submit className="mt-2 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            Filter
+          </Submit>
+        </Form>
+        <CasesCell contactEmail={contactEmail} />
+      </MainLayout>
     </>
   )
 }
